d34804f37926fb1f75e0c4bca4eb4c0d85053e55
Vlad1kavkaz <162821820v@gmail.com>
2024-12-28 00:23:59 +0300

    fix jenkinsfile in fin-core

--- fin-core/Jenkinsfile.groovy
+++ fin-core/Jenkinsfile.groovy
@@ -2,10 +2,11 @@ pipeline {
     agent any
 
     environment {
-        REPO_URL = 'https://github.com/Vlad1kavkaz/ci-cd-homework.git'
+        REPO_URL = 'https://github.com/Vlad1kavkaz/Transaction-control.git'
         DOCKER_IMAGE = 'fin-core'
         NETWORK_NAME = 'txn_control_jenkins_network'
         PORT_MAPPING = '8085:8085'
+        SERVICE_NAME = 'fin-core'
     }
 
     stages {
@@ -18,35 +19,35 @@ pipeline {
 
         stage('Compile and Build') {
             steps {
-                echo 'Compiling and building the project...'
-                sh 'mvn clean compile'
+                echo "Compiling and building the project in ${SERVICE_NAME}..."
+                sh "cd ${SERVICE_NAME} && mvn clean compile"
             }
         }
 
         stage('Run Unit Tests') {
             steps {
-                echo 'Running unit tests...'
-                sh 'mvn test'
+                echo "Running unit tests in ${SERVICE_NAME}..."
+                sh "cd ${SERVICE_NAME} && mvn test"
             }
         }
 
         stage('Package Application') {
             steps {
-                echo 'Packaging application...'
-                sh 'mvn package'
+                echo "Packaging application in ${SERVICE_NAME}..."
+                sh "cd ${SERVICE_NAME} && mvn package"
             }
         }
 
         stage('Build Docker Image') {
             steps {
-                echo 'Building Docker image...'
-                sh "docker build -t ${DOCKER_IMAGE} ."
+                echo "Building Docker image for ${SERVICE_NAME}..."
+                sh "docker build -t ${DOCKER_IMAGE} ${SERVICE_NAME}"
             }
         }
 
         stage('Run Docker Container') {
             steps {
-                echo 'Running Docker container...'
+                echo "Running Docker container for ${SERVICE_NAME}..."
                 sh "docker run -d --name ${DOCKER_IMAGE} --network ${NETWORK_NAME} -p ${PORT_MAPPING} ${DOCKER_IMAGE}"
             }
         }
