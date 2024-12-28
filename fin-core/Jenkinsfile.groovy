423b7a80aa80704a0a01048cfdae15ec6f955acd
Vlad1kavkaz <162821820v@gmail.com>
2024-12-28 11:52:22 +0300

    fix jenkins

--- fin-core/Jenkinsfile.groovy
+++ fin-core/Jenkinsfile.groovy
@@ -1,6 +1,10 @@
 pipeline {
     agent any
 
+    parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'Branch to build from')
+    }
+
     environment {
         REPO_URL = 'https://github.com/Vlad1kavkaz/Transaction-control.git'
         DOCKER_IMAGE = 'fin-core'
@@ -12,12 +16,12 @@ pipeline {
     stages {
         stage('Checkout Repository') {
             steps {
-                echo 'Cloning repository...'
-                git branch: 'master', url: "${REPO_URL}"
+                echo "Cloning repository from branch: ${BRANCH_NAME}..."
+                git branch: "${BRANCH_NAME}", url: "${REPO_URL}"
             }
         }
 
-        stage('Compile and Build') {
+        stage('Compile Build and Test') {
             steps {
                 echo "Compiling and building the project in ${SERVICE_NAME}..."
                 sh "cd ${SERVICE_NAME} && mvn clean package"
@@ -32,8 +36,11 @@ pipeline {
         }
 
         stage('Run Docker Container') {
+            when {
+                expression { params.BRANCH_NAME == 'master' }
+            }
             steps {
-                echo "Running Docker container for ${SERVICE_NAME}..."
+                echo "Running Docker container for ${SERVICE_NAME} on branch ${BRANCH_NAME}..."
                 sh "docker run -d --name ${DOCKER_IMAGE} --network ${NETWORK_NAME} -p ${PORT_MAPPING} ${DOCKER_IMAGE}"
             }
         }
