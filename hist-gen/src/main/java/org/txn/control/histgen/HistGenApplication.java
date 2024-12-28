808fd189c8fbe01ae85234d2d3884b27e5409f89
Vlad1kavkaz <162821820v@gmail.com>
2024-12-28 12:36:42 +0300

    init hist gen

---
+++ hist-gen/src/main/java/org/txn/control/histgen/HistGenApplication.java
@@ -0,0 +1,11 @@
+package org.txn.control.histgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistGenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HistGenApplication.class, args);
+    }
+}
